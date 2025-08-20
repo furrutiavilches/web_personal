c9461309a2e34e7a61e68a411070fa85d7aa45b3
furrutiavilches <f.urrutiavilches1@gmail.com>
2025-08-20 14:53:37 -0400

    Clean repo: remove all files

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("year").textContent = new Date().getFullYear();
-// Si publicas en GitHub Pages, setea el link de demo automáticamente:
-const user = "furrutiavilches"; const repo = "portafolio";
-document.getElementById("demoLink").href = `https://${user}.github.io/${repo}/`;
